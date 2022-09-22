2a12375203e3fc5322575ad451355794395a7d1b
Santiago Bavaresco <santiago.bavaresco@codere.com>
2022-09-22 13:27:58 -0300

    homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  //0011 = 3, se lee de izquierda a derecha
+  let suma = 0;
 
+    for (let i = 0; i < num.length; i++) {
+       suma += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return suma;
 }
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var res = "";
+  while(num != 0){
+    res = num % 2 + res;
+    num = Math.floor(num/2);
+  }
+  return res;
 }
 
 
